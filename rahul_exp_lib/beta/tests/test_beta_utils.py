eb00caaa240c0b01c51b60922022bc339686f49e
Rahul Kindi <rahulkindi@fb.com>
2021-09-25 09:57:02 -0400

    Added test __init__.py for both alpha and beta.

--- rahul_exp_lib/beta/tests/test_beta_utils.py
+++ rahul_exp_lib/beta/tests/test_beta_utils.py
@@ -5,7 +5,7 @@ from rahul_exp_lib.beta.beta_utils import mul
 
 class TestBeta(unittest.TestCase):
     def test_mul(self) -> None:
-        self.assertEquals(add(2, 3), 6)
+        self.assertEquals(mul(2, 3), 6)
         
 
 if __name__ == "__main__":
